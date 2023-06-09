45ae68fedc16e6739930a8fc3410c91e60c2a0f2
whitehall945 <whitehall945@gmail.com>
2023-06-09 14:45:34 +0800

    Day 3

--- leetcode/Solution.cpp
+++ leetcode/Solution.cpp
@@ -58,20 +58,49 @@ int Solution::uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid){
 		return 0;
 	}
 	vector<vector<int>>dp(m, vector<int>(n, 0));//dp[i][j]的含义是，从出发点到位置(i,j)的路径数量
-	for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {//列初始化
+	for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {//列初始化，有障碍的地方初始化为0
 		dp[i][0] = 1;
 	}
-	for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {//行初始化
+	for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {//行初始化，有障碍的地方初始化为0
 		dp[0][j] = 1;
 	}
 	for (int i = 1; i < m; i++) {//从左到右
 		for (int j = 1; j < n; j++) {//从上往下遍历
-			if (obstacleGrid[i][j] == 1)
+			if (obstacleGrid[i][j] == 1)//有障碍就保持这格为0
 				continue;
 			dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 		}
 	}
 	return dp[m - 1][n - 1];
+}
+
+int Solution::integerBreak(int n){//343. 整数拆分(m)
+	vector<int>dp(n + 1, 0);//dp[i]的含义是，数字i拆分相乘得到的最大值
+	dp[0] = 0;//初始化
+	dp[1] = 0;//0和1都认为不可拆分
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < i; j++) {//将j看作从i中拆分出去的第一个数
+			//dp[i]的取值可以由三种方式得到
+			//第一种，第二层循环运算的临时结果，当前最大值
+			//第二种，从i中拆分出j，且剩余部分i-j不再拆分，dp[i]的值即为j*(i-j)
+			//第三种，从i中拆分出j，且剩余部分i-j继续拆分，拆分相乘所得最大值在dp[i-j]中，
+			//		 dp[i]的值即为j*dp[i-j]
+			dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));	
+		}
+	}
+	return dp[n];
+}
+
+int Solution::numTrees(int n){//96. 不同的二叉搜索树(m)
+	vector<int>dp(n + 1, 0);
+	dp[0] = 1;//0个节点，不同BST有1个
+	dp[1] = 1;//1个节点，不同BST有1个
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			dp[i] += dp[j - 1] * dp[i - j];//状态转移方程见doc\dp.md\96
+		}
+	}
+	return dp[n];
 }
 
 int main() {
@@ -84,6 +113,11 @@ int main() {
 	//cout << solution.minCostClimbingStairs(cost) << endl;
 
 	//cout << solution.uniquePaths(7, 3) << endl;
-	vector<vector<int>>obstacleGrid = { {0, 0, 0},{0, 1, 0},{0, 0, 0} };
-	cout << solution.uniquePathsWithObstacles(obstacleGrid) << endl;
+	
+	//vector<vector<int>>obstacleGrid = { {0, 0, 0},{0, 1, 0},{0, 0, 0} };
+	//cout << solution.uniquePathsWithObstacles(obstacleGrid) << endl;
+
+	//cout << solution.integerBreak(10) << endl;
+
+	cout << solution.numTrees(3) << endl;
 }
--- leetcode/Solution.h
+++ leetcode/Solution.h
@@ -9,4 +9,6 @@ public:
 	int minCostClimbingStairs(vector<int>& cost);//746. 使用最小花费爬楼梯(e)
 	int uniquePaths(int m, int n);//62. 不同路径(m)
 	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid);//63. 不同路径 II(m)
+	int integerBreak(int n);//343. 整数拆分(m)
+	int numTrees(int n);//96. 不同的二叉搜索树(m)
 };
